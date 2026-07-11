2fead93bc3c48a8dc8d386557348d9a4ed850bac
BYuM <mkim997@gmail.com>
2026-07-11 11:25:56 +0900

    refactor) seperate initialize method

--- src/main.cpp
+++ src/main.cpp
@@ -11,62 +11,14 @@
 
 #include "camera_pins.h"
 
-unsigned long lastCaptureTime = 0; // Last shooting time
-int imageCount = 1;                // File Counter
-bool camera_sign = false;          // Check camera status
-bool sd_sign = false;              // Check sd status
-
+esp_err_t cameraInit();
+void sdInit();
+void photo_save(const char* fileName);
 void writeFile(fs::FS &fs, const char * path, uint8_t * data, size_t len);
 void wifiPostImageTask();
 
-// Save pictures to SD card
-void photo_save(const char* fileName) 
+esp_err_t cameraInit()
 {
-    // Take a photo
-    camera_fb_t *fb = esp_camera_fb_get();
-    if (!fb) 
-    {
-        Serial.println("Failed to get camera frame buffer");
-        return;
-    }
-
-    // Save photo to file
-    writeFile(SD, fileName, fb->buf, fb->len);
-    // Release image buffer
-    esp_camera_fb_return(fb);
-    Serial.println("Photo saved to file");
-}
-
-// SD card write file
-void writeFile(fs::FS &fs, const char * path, uint8_t * data, size_t len)
-{
-    Serial.printf("Writing file: %s\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file)
-    {
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if(file.write(data, len) == len)
-    {
-        Serial.println("File written");
-    } 
-    else 
-    {
-        Serial.println("Write failed");
-    }
-    file.close();
-}
-
-void setup() 
-{
-    Serial.begin(115200);
-    while(!Serial)
-        {delay(100);} // When the serial monitor is turned on, the program starts to execute
-
-    Serial.println("SETUP METHOD");
-
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
     config.ledc_timer = LEDC_TIMER_0;
@@ -121,15 +73,11 @@ void setup()
     }
 
     // camera init
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) 
-    {
-        Serial.printf("Camera init failed with error 0x%x", err);
-        return;
-    }
+    return esp_camera_init(&config);
+}
 
-    camera_sign = true; // Camera initialization check passes
-
+void sdInit()
+{
     // Initialize SD card
     if(!SD.begin(21))
     {
@@ -163,7 +111,63 @@ void setup()
         Serial.println("UNKNOWN");
     }
 
-    sd_sign = true; // sd initialization check passes
+}
+
+// Save pictures to SD card
+void photo_save(const char* fileName) 
+{
+    // Take a photo
+    camera_fb_t *fb = esp_camera_fb_get();
+    if (!fb) 
+    {
+        Serial.println("Failed to get camera frame buffer");
+        return;
+    }
+
+    // Save photo to file
+    writeFile(SD, fileName, fb->buf, fb->len);
+    // Release image buffer
+    esp_camera_fb_return(fb);
+    Serial.println("Photo saved to file");
+}
+
+// SD card write file
+void writeFile(fs::FS &fs, const char * path, uint8_t * data, size_t len)
+{
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file)
+    {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.write(data, len) == len)
+    {
+        Serial.println("File written");
+    } 
+    else 
+    {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void setup() 
+{
+    Serial.begin(115200);
+    while(!Serial)
+        {delay(100);} // When the serial monitor is turned on, the program starts to execute
+
+    Serial.println("SETUP METHOD");
+
+    if(cameraInit() != ESP_OK)
+    {
+        Serial.println("Failed to initialize camera");
+        return;
+    }
+
+    sdInit();
 
     Serial.println("Photos will begin in one minute, please be ready.");
 
@@ -171,20 +175,16 @@ void setup()
 
     for(int i = 0 ; i <= PHOTO_TO_CAPTURE ; i++)
     {
-        // Camera & SD available, start taking pictures
-        if(camera_sign && sd_sign)
+        // Get the current time
+        unsigned long now = millis();
+        int photoCount = 0;
+        char fileName[32] = "/Photo0.jpg";
+        while(SD.exists(fileName))
         {
-            // Get the current time
-            unsigned long now = millis();
-            int photoCount = 0;
-            char fileName[32] = "/Photo0.jpg";
-            while(SD.exists(fileName))
-            {
-                photoCount++;
-                sprintf(fileName,"/Photo%d.jpg",photoCount);
-            }
-            photo_save(fileName);
+            photoCount++;
+            sprintf(fileName,"/Photo%d.jpg",photoCount);
         }
+        photo_save(fileName);
         delay(10);
     }
 
